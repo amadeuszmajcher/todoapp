8780e9dda6c64c4daa7cb0f370fb5fed5f8c9202
Amadeusz Majcher <amadeuszmajcher@gmail.com>
2024-11-14 21:09:53 +0100

    adding a button component and styling

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,9 +1,16 @@
+import { Button } from "../Button/Button";
 import styles from "./Form.module.css";
 export function Form() {
   return (
     <form className={styles.form}>
       <input className={styles.input} type="text"></input>
-      <button className={styles.button}>Dodaj</button>
+      <Button
+        onClick={() => {
+          alert("test");
+        }}
+      >
+        Dodaj
+      </Button>
     </form>
   );
 }
