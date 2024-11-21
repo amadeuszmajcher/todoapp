d4c717c2cf1d1eb53f129c528d14891b01239650
Amadeusz Majcher <amadeuszmajcher@gmail.com>
2024-11-21 20:56:02 +0100

    styling the todoitem module

--- src/components/Todoitem/Todoitem.jsx
+++ src/components/Todoitem/Todoitem.jsx
@@ -4,7 +4,9 @@ import styles from "./Todoitem.module.css";
 export function Todoitem({ name, done }) {
   return (
     <li className={styles.item}>
-      <span className={styles.name}>{name}</span>
+      <span className={`${styles.name} ${done ? styles.done : ""}`}>
+        {name}
+      </span>
       {!done && <Button>Zrobione</Button>}
       <Button>Usuń</Button>
     </li>
